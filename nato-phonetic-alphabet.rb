cd434c16b1c5c0b238e0f8a6b0a104da289aed29
Mirco Macrelli <mail@mircomacrelli.net>
2019-07-07 08:14:20 +0200

    import

---
+++ nato-phonetic-alphabet.rb
@@ -0,0 +1,277 @@
+cheatsheet do
+  title 'NATO Phonetic Alphabet'
+  docset_file_name 'Nato_Phonetic_Alphabet'
+  keyword 'nato'
+
+  category do
+    id 'Letters'
+
+    header 'Character'
+    header 'Morse Code'
+    header 'Telephony'
+    header 'Phonic'
+
+    entry do
+      td_notes 'A'
+      td_notes '• —'
+      td_notes 'Alfa'
+      td_notes 'AL-FAH'
+      index_name 'A'
+    end
+    entry do
+      td_notes 'B'
+      td_notes '— • • •'
+      td_notes 'Bravo'
+      td_notes 'BRAH-VOH'
+      index_name 'B'
+    end
+    entry do
+      td_notes 'C'
+      td_notes '— • — •'
+      td_notes 'Charlie'
+      td_notes 'CHAR-LEE'
+      index_name 'C'
+    end
+    entry do
+      td_notes 'D'
+      td_notes '— • •'
+      td_notes 'Delta'
+      td_notes 'DELL-TAH'
+      index_name 'D'
+    end
+    entry do
+      td_notes 'E'
+      td_notes '•'
+      td_notes 'Echo'
+      td_notes 'ECK-OH'
+      index_name 'E'
+    end
+    entry do
+      td_notes 'F'
+      td_notes '• • — •'
+      td_notes 'Foxtrot'
+      td_notes 'FOCKS-TROT'
+      index_name 'F'
+    end
+    entry do
+      td_notes 'G'
+      td_notes '— — •'
+      td_notes 'Golf'
+      td_notes 'GOLF'
+      index_name 'G'
+    end
+    entry do
+      td_notes 'H'
+      td_notes '• • • •'
+      td_notes 'Hotel'
+      td_notes 'HOH-TEL'
+      index_name 'H'
+    end
+    entry do
+      td_notes 'I'
+      td_notes '• •'
+      td_notes 'India'
+      td_notes 'IN-DEE-AH'
+      index_name 'I'
+    end
+    entry do
+      td_notes 'J'
+      td_notes '• — — —'
+      td_notes 'Juliett'
+      td_notes 'JEW-LEE-ETT'
+      index_name 'J'
+    end
+    entry do
+      td_notes 'K'
+      td_notes '— • —'
+      td_notes 'Kilo'
+      td_notes 'KEY-LOH'
+      index_name 'K'
+    end
+    entry do
+      td_notes 'L'
+      td_notes '• — • •'
+      td_notes 'Lima'
+      td_notes 'LEE-MAH'
+      index_name 'L'
+    end
+    entry do
+      td_notes 'M'
+      td_notes '— —'
+      td_notes 'Mike'
+      td_notes 'MIKE'
+      index_name 'M'
+    end
+    entry do
+      td_notes 'N'
+      td_notes '— •'
+      td_notes 'November'
+      td_notes 'NO-VEM-BER'
+      index_name 'N'
+    end
+    entry do
+      td_notes 'O'
+      td_notes '— — —'
+      td_notes 'Oscar'
+      td_notes 'OSS-CAH'
+      index_name 'O'
+    end
+    entry do
+      td_notes 'P'
+      td_notes '• — — •'
+      td_notes 'Papa'
+      td_notes 'PAH-PAH'
+      index_name 'P'
+    end
+    entry do
+      td_notes 'Q'
+      td_notes '— — • —'
+      td_notes 'Quebec'
+      td_notes 'KEH-BECK'
+      index_name 'Q'
+    end
+    entry do
+      td_notes 'R'
+      td_notes '• — •'
+      td_notes 'Romeo'
+      td_notes 'ROW-ME-OH'
+      index_name 'R'
+    end
+    entry do
+      td_notes 'S'
+      td_notes '• • •'
+      td_notes 'Sierra'
+      td_notes 'SEE-AIT-RAH'
+      index_name 'S'
+    end
+    entry do
+      td_notes 'T'
+      td_notes '—'
+      td_notes 'Tango'
+      td_notes 'TAN-GO'
+      index_name 'T'
+    end
+    entry do
+      td_notes 'U'
+      td_notes '• • —'
+      td_notes 'Uniform'
+      td_notes 'YOU-NEE-FORM'
+      index_name 'U'
+    end
+    entry do
+      td_notes 'V'
+      td_notes '• • • —'
+      td_notes 'Victor'
+      td_notes 'VIK-TAH'
+      index_name 'V'
+    end
+    entry do
+      td_notes 'W'
+      td_notes '• — —'
+      td_notes 'Whiskey'
+      td_notes 'WISS-KEY'
+      index_name 'W'
+    end
+    entry do
+      td_notes 'X'
+      td_notes '— • • —'
+      td_notes 'Xray'
+      td_notes 'ECKS-RAY'
+      index_name 'X'
+    end
+    entry do
+      td_notes 'Y'
+      td_notes '— • — —'
+      td_notes 'Yankee'
+      td_notes 'YANG-KEY'
+      index_name 'Y'
+    end
+    entry do
+      td_notes 'Z'
+      td_notes '— — • •'
+      td_notes 'Zulu'
+      td_notes 'ZOO-LOO'
+      index_name 'Z'
+    end
+  end
+
+  category do
+    id 'Numbers'
+
+    header 'Character'
+    header 'Morse Code'
+    header 'Telephony'
+    header 'Phonic'
+
+    entry do
+      td_notes '0'
+      td_notes '— — — — —'
+      td_notes 'Zero'
+      td_notes 'ZEE-RO'
+      index_name '0'
+    end
+    entry do
+      td_notes '1'
+      td_notes '• — — — —'
+      td_notes 'One'
+      td_notes 'WUN'
+      index_name '1'
+    end
+    entry do
+      td_notes '2'
+      td_notes '• • — — —'
+      td_notes 'Two'
+      td_notes 'TOO'
+      index_name '2'
+    end
+    entry do
+      td_notes '3'
+      td_notes '• • • — —'
+      td_notes 'Three'
+      td_notes 'TREE'
+      index_name '3'
+    end
+    entry do
+      td_notes '4'
+      td_notes '• • • • —'
+      td_notes 'Four'
+      td_notes 'FOW-ER'
+      index_name '4'
+    end
+    entry do
+      td_notes '5'
+      td_notes '• • • • •'
+      td_notes 'Five'
+      td_notes 'FIFE'
+      index_name '5'
+    end
+    entry do
+      td_notes '6'
+      td_notes '— • • • •'
+      td_notes 'Six'
+      td_notes 'SIX'
+      index_name '6'
+    end
+    entry do
+      td_notes '7'
+      td_notes '— — • • •'
+      td_notes 'Seven'
+      td_notes 'SEV-EN'
+      index_name '7'
+    end
+    entry do
+      td_notes '8'
+      td_notes '— — — • •'
+      td_notes 'Eight'
+      td_notes 'AIT'
+      index_name '8'
+    end
+    entry do
+      td_notes '9'
+      td_notes '— — — — •'
+      td_notes 'Nine'
+      td_notes 'NIN-ER'
+      index_name '9'
+    end
+  end
+end
